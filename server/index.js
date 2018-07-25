26d425164bfbc2f423703b01ede65f8f8a25559c
Jesse <rank_booster@yahoo.com>
2018-07-25 14:54:59 -0600

    thru step 4

--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.get('/callback', (req, res) => {
-
-  // Code below
-
+  exchangeCodeForAccessToken()
+    .then(exchangeAccessTokenForUserInfo)
+    .then(fetchAuth0AccessToken)
+    .then(fetchGitHubAccessToken)
+    .then(setGitTokenToSession)
+    .catch(error => {
+      console.log('Server error', error);
+      res.status(500).send('An error occurred on the server. Check the terminal.');
+    });
 })
 
 app.get('/api/user-data', (req, res) => {
